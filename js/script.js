fefde123ac323a183da752b73a5074c6ef211d85
K.Adam White <kadamwhite@gmail.com>
2012-01-27 21:29:29 -0500

    Print device orientation info onto the screen

--- js/script.js
+++ js/script.js
@@ -1,13 +1,20 @@
 /* Author: K.Adam White & Will Blair
 */
-
-if (window.DeviceMotionEvent==undefined) {
-	$('body').html('<h1>You should be using a GOD DAMNED PHONE</h1>');
-} 
-else {
-	window.ondeviceorientation = function(event) {
-		alpha = Math.round(event.alpha);
-		beta = Math.round(event.beta);
-		gamma = Math.round(event.gamma);
-	}
+if (window.DeviceOrientationEvent && window.DeviceMotionEvent) {
+  window.ondeviceorientation = function(event) {
+    alpha = Math.round(event.alpha);
+    beta = Math.round(event.beta);
+    gamma = Math.round(event.gamma);
+    $('body').html([
+      '<p>alpha: ',
+      alpha,
+      '<br />beta: ',
+      beta,
+      '<br />gamma: ',
+      gamma,
+      '</p>'
+    ].join(''));
+  }
+} else {
+  $('body').html('<h1>You should be using a GOD DAMNED PHONE</h1>');
 }
