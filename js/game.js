930ecf2b2d1bbb443d77d21381284e3513a966fd
K.Adam White <kadamwhite@gmail.com>
2012-01-28 11:37:49 -0500

    Added Drink and Ingredient constructors; broke data model and game logic out into different files

---
+++ js/game.js
@@ -0,0 +1,90 @@
+/* Author: K.Adam White & Will Blair
+ */
+
+hangover = {
+    isPouring: false,
+    currentIngredient: null,
+    pour: {
+        start: 0,
+        end: 0
+    },
+    drink: {
+        name: "foo",
+        ingredients: [
+            {name: "bar", oz: 1},
+            {name: "baz", oz: 2}
+        ]
+    },
+    processPour: function() {
+        var pour = hangover.pour;
+        var diff = pour.pourEnd - pour.PourStart;//in ms
+    }
+};
+/**
+ * Return the length of the pour event, in milliseconds
+ */
+hangover.pour.time = function() {
+    return this.end - this.start;
+};
+
+
+if( window.DeviceOrientationEvent && window.DeviceMotionEvent ) {
+    window.ondeviceorientation = function(event) {
+        alpha = Math.round(event.alpha);
+        beta = Math.round(event.beta);
+        gamma = Math.round(event.gamma);
+        /* Update values every 10th of a second */
+        //setInterval(function(){
+        $('#debug #state').html([
+            'alpha: ',
+            alpha,
+            '<br />beta: ',
+            beta,
+            '<br />gamma: ',
+            gamma
+        ].join(''));
+        //}, 100);
+
+        if(
+            (100 <= alpha && alpha <= 270)
+        //&& (-30 <= beta && beta <= 0)
+        //&& (-70 <= gamma && gamma <= 70)
+            ) {
+            //Start pouring
+            if( !hangover.isPouring ) {
+                hangover.isPouring = true;
+                hangover.pour.start = new Date();
+                $('#debug #pouring').html('POURING');
+            }
+        } else {
+            //Done pouring
+            if( hangover.isPouring ) {
+                hangover.isPouring = false;
+                hangover.pour.end = new Date();
+                $('#debug #pouring').html('');
+            }
+        }
+    }
+} else {
+    //$('body').html('<h1>You should be using a GOD DAMNED PHONE</h1>');
+}
+
+
+/*
+ Z axis - Positive perpendicular of the phone's screen
+ Y axis - Positive towards the top of the phone
+ X axis - Positive towards the right side of the screen.
+
+ Alpha - Rotation around the z axis. [0,360)
+ right - <=270 degrees
+ left  - >=100 degrees
+
+ Beta - Rotation around the x axis [-180,180)
+ left -  >= -30
+ right-  <= 0
+
+ Gamma - Rotation around the y axis [-90,90]
+
+ left -  <= 70
+ right - >= -70
+ */
--- js/script.js
+++ js/script.js
@@ -1,89 +1,58 @@
-/* Author: K.Adam White & Will Blair
+/* Author: K.Adam White
  */
-hangover = {
-    isPouring:false,
-    currentIngredient:null,
-    pour:{
-        start:0,
-        end:0
-    },
-    drink:{
-        name:"foo",
-        ingredients:[
-            {name:"bar", oz:1},
-            {name:"baz", oz:2}
-        ]
-    },
-    processPour:function () {
-        var pour = hangover.pour;
-        var diff = pour.pourEnd - pour.PourStart;//in ms
+
+/**
+ * Creates an instance of Drink.
+ * @constructor
+ * @this {Drink}
+ * @param {String} name String value containing the name of the cocktail
+ * @param {Ingredient[]} ingredients Array of Ingredients objects
+ */
+function Drink(name, ingredients) {
+    // Guard against invocation of Drink() without the new keyword
+    if( !(this instanceof Drink) ) {
+        return new Drink(name, ingredients);
     }
+    this.name = name;
+    this.ingredients = ingredients;
+
+    return this; // Implied, but useful for readability
+}
+/**
+ * Returns the total volume of the Drink, in ounces
+ */
+Drink.prototype.volume = function() {
+    var i = 0, // For-loop counter
+        max = this.complexity(), // Number of ingredients
+        totalVolume = 0; // Running total
+    for( i; i++; i < max ) {
+        totalVolume = totalVolume + this.ingredients[i].oz;
+    }
+    return totalVolume;
 };
 /**
- * Return the length of the pour event, in milliseconds
+ * Returns the number of ingredients in the Drink
  */
-hangover.pour.time = function () {
-    return this.end - this.start;
-};
-
-
-if ( window.DeviceOrientationEvent && window.DeviceMotionEvent ) {
-    window.ondeviceorientation = function (event) {
-        alpha = Math.round(event.alpha);
-        beta = Math.round(event.beta);
-        gamma = Math.round(event.gamma);
-        /* Update values every 10th of a second */
-        //setInterval(function(){
-        $('#debug #state').html([
-            'alpha: ',
-            alpha,
-            '<br />beta: ',
-            beta,
-            '<br />gamma: ',
-            gamma
-        ].join(''));
-        //}, 100);
-
-        if (
-            (100 <= alpha && alpha <= 270)
-        //&& (-30 <= beta && beta <= 0)
-        //&& (-70 <= gamma && gamma <= 70)
-            ) {
-            //Start pouring
-            if ( !hangover.isPouring ) {
-                hangover.isPouring = true;
-                hangover.pour.start = new Date();
-                $('#debug #pouring').html('POURING');
-            }
-        } else {
-            //Done pouring
-            if ( hangover.isPouring ) {
-                hangover.isPouring = false;
-                hangover.pour.end = new Date();
-                $('#debug #pouring').html('');
-            }
-        }
-    }
-} else {
-    //$('body').html('<h1>You should be using a GOD DAMNED PHONE</h1>');
+Drink.prototype.complexity = function() {
+    return this.ingredients.length;
 }
 
-
-/*
- Z axis - Positive perpendicular of the phone's screen
- Y axis - Positive towards the top of the phone
- X axis - Positive towards the right side of the screen.
-
- Alpha - Rotation around the z axis. [0,360)
- right - <=270 degrees
- left  - >=100 degrees
-
- Beta - Rotation around the x axis [-180,180)
- left -  >= -30
- right-  <= 0
-
- Gamma - Rotation around the y axis [-90,90]
-
- left -  <= 70
- right - >= -70
+/**
+ * Creates an instance of Ingredient
+ * @constructor
+ * @this {Ingredient}
+ * @param {String} name The name of the ingredient.
+ * @param {Number} quantityInOunces The amount of the ingredient to use
+ * TODO: find a way to make sure this value matches other drinks with the same ingredient
  */
+function Ingredient(name, quantityInOunces) {
+    // Guard against invocation of Ingredient() without the new keyword
+    if( !(this instanceof Ingredient) ) {
+        return new Ingredient(name, quantityInOunces);
+    }
+    // Format our strings as a nice object literal
+    return {
+        name: name,
+        oz: quantityInOunces
+    };
+}
