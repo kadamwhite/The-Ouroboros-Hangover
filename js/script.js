5dc06c463a3b983277f060bc064be4dca4184f2e
K.Adam White <kadamwhite@gmail.com>
2012-01-27 21:21:38 -0500

    Fix modernizr exception, add GA tracking code, get a scaffold of JS orientation detection working

--- js/script.js
+++ js/script.js
@@ -1,26 +1,13 @@
-/* Author: 
-
+/* Author: K.Adam White & Will Blair
 */
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if (window.DeviceMotionEvent==undefined) {
+	$('body').html('<h1>You should be using a GOD DAMNED PHONE</h1>');
+} 
+else {
+	window.ondeviceorientation = function(event) {
+		alpha = Math.round(event.alpha);
+		beta = Math.round(event.beta);
+		gamma = Math.round(event.gamma);
+	}
+}
